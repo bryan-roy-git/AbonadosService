19dd43ff14259e3e8d0a04a4c3efb4509f82977c
Bryan <rchongcervantes@gmail.com>
2021-11-28 20:01:21 +0100

    Services estable

--- app/Http/Controllers/api/AbonadoController.php
+++ app/Http/Controllers/api/AbonadoController.php
@@ -15,6 +15,7 @@ use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use App\Http\Requests\AbonadoRulesController;
 
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class AbonadoController extends ApiResponseController
 {
@@ -33,7 +34,8 @@ class AbonadoController extends ApiResponseController
                         DB::raw("CONCAT('".url('/qrcodes')."/',abonados.qr) AS qr"),
                         DB::raw("CONCAT('".url('/abonados')."/',abonados.foto) AS foto"))
                     ->with('tarifa')
-                    ->get();
+                    // ->get();
+                    ->paginate(10);
         return $this->successResponse($abonados);
 
         // return $abonados;
@@ -57,7 +59,8 @@ class AbonadoController extends ApiResponseController
      */
     public function store(Request $request)
     {
-        // dd($request->all());
+        // dd($request->foto);
+        // var_dump($request->all());
         $validator = Validator::make($request->all(), AbonadoRulesController::abonadoRules());
 
         if ($validator->fails()) {
@@ -66,10 +69,20 @@ class AbonadoController extends ApiResponseController
         }else{
             $new_abonado = $request->all();
             $hash= md5($request->nif."-".time());
-            
-            $filename = $hash.".".$request->foto->extension(); // ----> NAME FILE FOTO
-            $request->foto->move(public_path('abonados'),$filename);
+
+            // ------------------> BASE64 FORMAT <-------------------------
+            // $filename = $hash."."."png"; // ----> NAME FILE FOTO
+            // $filename = $hash.".".$request->foto->extension(); // ----> NAME FILE FOTO
+            // $request->foto->move(public_path('abonados'),$filename);
+            // $new_abonado["foto"] = $filename;
+            $base64_str = substr($request->foto, strpos($request->foto, ",")+1); 
+            $extension = explode('/', mime_content_type($request->foto))[1]; //EXTENSION DEL ARCHIVO
+
+            $image = base64_decode($base64_str);
+            $filename = $hash.".".$extension;
+            file_put_contents(public_path()."\abonados\\".$filename,$image);
             $new_abonado["foto"] = $filename;
+
 
             $const = self::CONSTANTS;
             $qrFile = $hash.$const['qrExtension']; // ----> NAME FILE QrCode
@@ -137,21 +150,32 @@ class AbonadoController extends ApiResponseController
     public function update(Request $request)
     {
         //
+        // dd($request->all());
         $id = $request->id;
         $validator = Validator::make($request->all(), AbonadoRulesController::updateRules($id));
         if ($validator->fails()) {
             return $this->errorValidateResponse($validator->errors());
         }else{
             $abonado = Abonado::where('id',$id)->first();
-
+            // dd($request->foto);
             if ($request->foto) {
-                $hash= md5($request->nif."-".time()).".".$request->foto->extension();
+
+                // $hash= md5($request->nif."-".time()).".".$request->foto->extension();
+                // File::delete(public_path('abonados/'.$abonado->foto));
+                // $request->foto->move(public_path('abonados'),$hash);
                 File::delete(public_path('abonados/'.$abonado->foto));
-                $request->foto->move(public_path('abonados'),$hash);
-                
+
+                $hash= md5($request->nif."-".time());
+                $base64_str = substr($request->foto, strpos($request->foto, ",")+1);
+                $extension = explode('/', mime_content_type($request->foto))[1];
+
+                $image = base64_decode($base64_str);
+                $filename = $hash.".".$extension;
+
+                file_put_contents(public_path()."\abonados\\".$filename,$image);
+
                 $new_values = $request->all();
-                $new_values["foto"] = $hash;
-                // dd($new_values);
+                $new_values["foto"] = $filename;
                 $abonado->update($new_values);
 
             }else{
@@ -159,9 +183,7 @@ class AbonadoController extends ApiResponseController
             }
                 $mapAbonado = $this->mapAbonado($abonado);
             return $this->successResponse($mapAbonado,"Abonado actualizado correctamente");
-
         }
-  
     }
     /**
      * Remove the specified resource from storage.
@@ -176,7 +198,7 @@ class AbonadoController extends ApiResponseController
         File::delete(public_path('abonados/'.$abonado->foto));
         File::delete(public_path('qrcodes/'.$abonado->qr));
         $abonado->delete();
-        return $this->successResponse("Eliminado correctamente");
+        return $this->successResponse($abonado,"Eliminado correctamente");
     }
 
     public function search(Request $request)
@@ -189,7 +211,7 @@ class AbonadoController extends ApiResponseController
             'apellidos',
             'telefono',
             'email',
-            'numeo_abonado',
+            'numero_abonado',
             'estado',
             'id_tarifa',
             'pagado_tarifa',
@@ -210,8 +232,25 @@ class AbonadoController extends ApiResponseController
         }        
         $abonados = $abonados->with('tarifa')->get();
         return $this->successResponse($abonados);
-
     }
 
+
+
+    // public function postTest(Request $request) {
+    //     $data = $request;
+    //     $base64_str = substr($data->base64_image, strpos($data->base64_image, ",")+1);
+
+    //     $extension = explode('/', mime_content_type($data->base64_image))[1];
+        
+    //     dd($base64_str,$extension);
+    //     $image = base64_decode($base64_str);
+
+    //     $hash = "img.png";
+    //     file_put_contents(public_path()."\abonados\\".$hash,$image);
+    //     $response = array(
+    //         'status' => 'success',
+    //     );
+    //     return $this->successResponse( $response ," succes" );
+    // }
 
 }
